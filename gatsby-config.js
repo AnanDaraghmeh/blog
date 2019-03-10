fe3d5fd0b8bd81b8f7cc047eff9c338bb1f61b63
AnanDaraghmeh <anan.daraghmeh@yahoo.com>
2019-03-10 22:40:32 +0100

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,7 +58,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-135936892-2`,
       }
     },
     `gatsby-plugin-feed`,
