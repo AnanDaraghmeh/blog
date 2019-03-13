4640c403e946dd73e2b20a0e3123b691536f9a70
AnanDaraghmeh <anan.daraghmeh@yahoo.com>
2019-03-13 11:46:01 +0100

    update favicon

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -8,6 +8,7 @@ function Footer() {
       `}
     >
       © {new Date().getFullYear()}, React For Cats.
+      <div>Favicon is made by <a href="https://www.freepik.com/" target="_blank">Freepik</a> from <a href="https://www.flaticon.com/" target="_blank">www.flaticon.com</a> and licensed by <a href="http://creativecommons.org/licenses/by/3.0/" target="_blank">CC BY 3.0.</a></div>
     </footer>
   );
 }
